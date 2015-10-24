1e34da445d529bce798f047b5bad9990a985071a
kemalbuyukkaya <kemalbuyukkaya@gmail.com>
2015-10-24 12:17:12 +0300

    Interface is roughly done.

--- src/GameKit.java
+++ src/GameKit.java
@@ -1,2 +1,42 @@
+import java.awt.*;
+
 public class GameKit {
+
+    protected int x;
+    protected int y;
+    protected int width;
+    protected int height;
+    protected Image image;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    Image getImage() {
+        return image;
+    }
+
+    Rectangle getRect() {
+        return new Rectangle(x, y, image.getWidth(null), image.getHeight(null));
+    }
 }
