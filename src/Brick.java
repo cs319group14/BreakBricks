9fb8d3a6b32aa82b99f4c1e5618452a01c4dc080
kemalbuyukkaya <kemalbuyukkaya@gmail.com>
2015-10-26 20:54:32 +0300

    Brick, Ball and Paddle is done.

--- src/Brick.java
+++ src/Brick.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
+
 public class Brick extends GameKit implements BreakBricksCommons {
+
+    String brickImageDirectory = "";
+    boolean destroyed;
+
+    public Brick(int x, int y) {
+        this.x = x;
+        this.y = y;
+        ImageIcon ii = new ImageIcon(brickImageDirectory);
+        image = ii.getImage();
+        width = image.getWidth(null);
+        height = image.getHeight(null);
+        destroyed = false;
+    }
+
+    public boolean isDestroyed() {
+        return destroyed;
+    }
+
+    public void setDestroyed(boolean destroyed) {
+        this.destroyed = destroyed;
+    }
 }
