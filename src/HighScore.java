0741f6b278fbf6ba4f5730f027c8c608312d32f5
kemalbuyukkaya <kemalbuyukkaya@gmail.com>
2015-10-26 23:40:22 +0300

    High Score support is added.

--- src/HighScore.java
+++ src/HighScore.java
@@ -1,2 +1,34 @@
-public class HighScore {
+import javax.swing.*;
+
+public class HighScore extends JPanel implements BreakBricksCommons {
+
+    int[] hS;//sample array
+    String output;
+    JTextField myL;
+
+    public HighScore() {
+        output = "";
+        hS = new int[10];
+        for (int i = 0; i < 10; i++) {
+            hS[i] = i;
+        }
+        convert();
+
+        myL = new JTextField(output);
+        System.out.println(myL.getText());
+        this.add(myL);
+        myL.setVisible(true);
+
+
+        setSize(BreakBricksCommons.WIDTH, BreakBricksCommons.HEIGTH);
+        setIgnoreRepaint(true);
+        setVisible(true);
+        repaint();
+    }
+
+    public void convert() {
+        for (int i = 0; i < 10; i++) {
+            output = output + hS[i] + " /n";
+        }
+    }
 }
