737e118a6c3eaa277e858b9be3d4f3ddb2075808
Kaan <aknckaan1@gmail.com>
2015-10-27 11:28:21 +0300

    added unpause to board

--- src/BreakBricks.java
+++ src/BreakBricks.java
@@ -1,15 +1,5 @@
 import javax.swing.*;
 
 public class BreakBricks extends JFrame{
-
-
-    public BreakBricks()
-    {
-        add(new Board());
-    }
-
-    public static void main(String [ ] args)
-    {
-        new BreakBricks();
-    }
+    
 }
