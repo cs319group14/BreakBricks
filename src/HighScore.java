f157025465f592b83c6e4f7511ee093303b66088
Kaan <aknckaan1@gmail.com>
2015-12-08 23:56:26 +0200

    database

--- src/HighScore.java
+++ src/HighScore.java
@@ -10,46 +10,55 @@ public class HighScore extends JPanel implements BreakBricksCommons {
     public JButton backButton;
     public HighScore() throws IOException {
 
-        firstLabel = new JLabel();
-        secondLabel = new JLabel();
-        thirdLabel = new JLabel();
+        firstLabel = new JLabel("-");
+        secondLabel = new JLabel("-");
+        thirdLabel = new JLabel("-");
         hsLabel = new JLabel();
 
+
+
         backButton=new JButton("Back");
-        backButton.setSize(new Dimension(30,30));
+        backButton.setSize(new Dimension(50,50));
         backButton.setVisible(true);
 
-        firstLabel.setSize(new Dimension(300,300));
+        this.setLayout(new GridLayout());
+
+
+        firstLabel.setSize(new Dimension(100,100));
         firstLabel.setText("First Label ");
         firstLabel.setVisible(true);
 
-        secondLabel.setSize(new Dimension(300,300));
+        secondLabel.setSize(new Dimension(50,50));
         secondLabel.setText("Second Label ");
         secondLabel.setVisible(true);
 
-        thirdLabel.setSize(new Dimension(300,300));
+        thirdLabel.setSize(new Dimension(50,50));
         thirdLabel.setText("Third Label ");
         thirdLabel.setVisible(true);
 
-        hsLabel.setSize(new Dimension(300,300));
+        hsLabel.setSize(new Dimension(50,50));
         hsLabel.setText("High Scores ");
         hsLabel.setVisible(true);
 
-        this.setLayout(new BorderLayout());
         this.add(firstLabel);
-        this.add(secondLabel);
-        this.add(thirdLabel);
-        this.add(backButton);
-        repaint();
-        refreshHS();
+        //this.add(secondLabel);
+        //this.add(thirdLabel);
+        //this.add(backButton);
 
+
+        refreshHS();
+        repaint();
     }
 
     public void refreshHS() throws IOException {
+        db.storeScore(100);
+        db.storeScore(90);
+        db.storeScore(80);
         try{
             firstLabel.setText((String) db.getAllScores().get(0));
             secondLabel.setText((String) db.getAllScores().get(1));
             thirdLabel.setText((String) db.getAllScores().get(2));
+            repaint();
         }catch (NullPointerException e)
         {
             System.out.println("Null database index");
--- src/dbManager.java
+++ src/dbManager.java
@@ -17,37 +17,56 @@ public class dbManager {
 
     public ArrayList getAllScores() throws IOException {
 
+        ArrayList arr= new ArrayList();
+
         File f = new File(addres);
         if(f.exists())
         {
             FileReader input = new FileReader(f);
             BufferedReader bufRead = new BufferedReader(input);
+
+            String myLine="-";
+
+            while((myLine = bufRead.readLine()) != null)
+            {
+                arr.add(myLine);
+            }
+
+            for(int i=arr.size();i<3;i++)
+            {
+                arr.add("-");
+            }
+            bufRead.close();
+            input.close();
+
+            return arr;
         }
         else
         {
             System.out.println("File: " + addres + " not found, creating a new one.");
             f.createNewFile();
-
+            return null;
         }
-        return null;
+        //return null;
     }
 
     public void storeScore(int score) throws IOException {
         //writes score to the txt file
-
         File f=new File(addres);
+
         if(!f.exists())
         {
             f.createNewFile();
             System.out.println("File: " + addres + " not found, creating a new one.");
         }
+
         System.out.println("Data saved.");
-
-        FileWriter input = new FileWriter(f);
-        BufferedWriter bufRead = new BufferedWriter(input);
-        bufRead.newLine();
-        bufRead.write(score);
-
+        FileWriter input = new FileWriter(f.getAbsolutePath());
+        BufferedWriter bufWrite = new BufferedWriter(input);
+        bufWrite.newLine();
+        bufWrite.write(score+"");
+        bufWrite.close();
+        input.close();
 
     }
 }
